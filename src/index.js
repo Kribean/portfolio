aa0e0f437544fb481e462f50a5afed0b57eba89b
Ramael <ramael.bruno@gmail.com>
2022-03-22 23:12:10 +0100

    correctoin de la troisieme section, ajout de commentaire

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-let x = document.getElementsByClassName('heart');
-let divProjet = document.getElementById('projet');
-let API_URL = 'https://api.github.com/users/Kribean/repos#list-repositories-for-a-user';
-let tableau=[];
+let x = document.getElementsByClassName("heart");
+let divProjet = document.getElementById("projet");
+let API_URL =
+  "https://api.github.com/users/Kribean/repos#list-repositories-for-a-user";
 
-
-
-for(let i =0;i<x.length;i++)
-{
-  x[i].addEventListener('click',(event)=>{
+/*Permet d'activer la classe permettant d'activer le coeur */
+for (let i = 0; i < x.length; i++) {
+  x[i].addEventListener("click", (event) => {
     x[i].classList.toggle("is-active");
-});
+  });
 }
 
-
+/*recherche dans github les projets de R. Bruno*/
 fetch(API_URL, {
   method: "GET",
   headers: {
@@ -24,19 +22,15 @@ fetch(API_URL, {
     return result.json();
   })
   .then((tableauMessages) => {
-    this.tableau = tableauMessages;
-    console.log(this.tableau);
-    tableauMessages.forEach(element => {
-      divProjet.innerHTML+=`<div class="card m-2" style="width: 18rem;">
+    tableauMessages.forEach((element) => {
+      divProjet.innerHTML += `<div class="card m-2" style="width: 18rem;">
       <div class="card-body">
-        <h5 class="card-title">${element.name}</h5>
+        <h3 class="card-title">${element.name}</h3>
         <p class="card-text">Le language principale utilisé pour ce projet est ${element.language}</p>
-        <h5>Description du projet</h5>
+        <h3>Description du projet</h3>
         <p>${element.description}</p>
         <a href="${element.html_url}" class="btn btn-success h3" target="_blank">Accéder au projet</a>
       </div>
-    </div>`
+    </div>`;
     });
   });
-
-  
